fc424155a88142d8c01d961cf32783b59db81925
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-12-03 21:49:53 +0600

    Deploy on Netlify

--- express/server.js
+++ express/server.js
@@ -4,14 +4,6 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
 
 var voter=[];
 
