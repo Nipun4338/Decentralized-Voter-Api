96cc742bf03866a2509a257d2762b5a9d7c81991
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-12-03 22:23:07 +0600

    Deploy on Netlify

--- express/server.js
+++ express/server.js
@@ -28,6 +28,7 @@ router.get('/', (req, res) => {
   res.send(voter);
 });
 
+app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.send(voter));
 
