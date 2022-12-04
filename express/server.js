49a38f08787ba03140f57d6fd5b61cb2818e6e44
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-12-04 11:13:33 +0600

    Changing wai of api

--- express/server.js
+++ express/server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../json.json')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
